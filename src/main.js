31efe817d01a007cf5795442176de346f2fc45de
Mahmuddin Nurul Fajri <mahmuddinnf@gmail.com>
2023-04-20 10:36:15 +0700

    [main] 20 Apr 2023 - Install dan Konfigurasi Route di Vue 3

--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//import config router
+import router from './router'
+
+createApp(App).use(router).mount('#app')
